b7277ad39fd88e5b4c5eaa6b3df561ba4ecb4e9b
Zhan Cao <zcao@mdsol.com>
2023-01-20 13:27:19 -0500

    Housekeeping: update antlr to 4.6.11 and use antlr4 instead of java directly

--- demoGrammar/step2_compileDll.bat
+++ demoGrammar/step2_compileDll.bat
@@ -1,3 +1,3 @@
 @echo Restore the packages first (by building project), then run this. C# compiler Csc.exe should be in your path.
 
-csc /target:library /out:demo.dll /reference:.\..\packages\Antlr4.Runtime.Standard.4.6.0\lib\net35\Antlr4.Runtime.Standard.dll demo*.cs
+csc /target:library /out:demo.dll /reference:.\..\packages\Antlr4.Runtime.Standard.4.11.1\lib\\net45\Antlr4.Runtime.Standard.dll demo*.cs
